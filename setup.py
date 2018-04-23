db2ddb25a61c3547fef09cd702684df38717b73a
salimm <salimm@cs.pitt.edu>
2018-04-22 23:23:20 -0400

    changed to allow Dictionary contain mapper_backend

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup
 setup(
   name = 'pylods-json',
   packages = ['pylodsjson'], # this must be the same as the name above
-  version = '0.3.5',
+  version = '0.3.6',
   description = 'json extension for pylods msgpack',
   author = 'Salim Malakouti',
   author_email = 'salim.malakouti@gmail.com',
   license = 'MIT',
   url = 'https://github.com/salimm/pylods-ijson', # use the URL to the github repo
-  download_url = 'http://github.com/salimm/pylods-json/archive/0.3.5.tar.gz', # I'll explain this in a second
+  download_url = 'http://github.com/salimm/pylods-json/archive/0.3.6.tar.gz', # I'll explain this in a second
   keywords = ['python','serialization','deserialization','paser','json','object oriented','fast','extendable','type based','jackson json', 'ijson'], # arbitrary keywords
   classifiers = ['Programming Language :: Python'],
   install_requires=['pylods','ijson'],
